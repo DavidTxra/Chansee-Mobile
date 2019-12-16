276ddde76922fc1d1da723f52faf0e369ca472bb
DavidTxra <david.teixeira@edu.devinci.fr>
2019-12-16 20:57:20 +0100

    feat: updates

--- utilities/UserPermissions.js
+++ utilities/UserPermissions.js
@@ -3,7 +3,7 @@ import * as Permissions from 'expo-permissions'
 
 class UserPermissions {
     getCameraPermission = async () => {
-        if(Constants.plateform.ios) {
+        if(Constants.platform.ios) {
             const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
 
             if(status != "granted") {
