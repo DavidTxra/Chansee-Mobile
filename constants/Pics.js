26cf67c3493be1026dfe27019990d24b5fc3350e
ProLyfe <Rayane.prolyfe@gmail.com>
2020-01-05 22:11:43 +0100

    Feat: swipe system

---
+++ constants/Pics.js
@@ -0,0 +1,104 @@
+import shuffleArray from '../utils/shuffleArray'
+
+export const HomeScreenPics = shuffleArray([
+  {
+    pic: require('../assets/images/women1.jpg'),
+    title: 'Amelia, Attaquante',
+    caption: "Le foot c'est trop cool",
+  },
+  {
+    pic: require('../assets/images/women2.jpg'),
+    title: 'Joanna, Attaquante',
+    caption: 'Qui est chaud pour des 2vs2 ?',
+  },
+  {
+    pic: require('../assets/images/women3.jpg'),
+    title: 'Charlie, Attaquante',
+    caption: 'On me surnomme foot girl',
+  },
+  {
+    pic: require('../assets/images/women4.jpg'),
+    title: 'Mary, Attaquante',
+    caption: 'Joueuse du dimanche :)',
+  },
+  {
+    pic: require('../assets/images/women5.jpg'),
+    title: 'Lucy, Attaquante',
+    caption: "Je veux trop intégrer l'équipe de foot féminine :o",
+  },
+  {
+    pic: require('../assets/images/women6.jpg'),
+    title: 'Rachel, Défenseuse',
+    caption: 'Yeeeeees',
+  },
+  {
+    pic: require('../assets/images/women7.jpg'),
+    title: 'Ava, Défenseuse',
+    caption: "rdv à l'aube",
+  },
+  {
+    pic: require('../assets/images/women8.jpg'),
+    title: 'Monica, Défenseuse',
+    caption: 'je vous fume',
+  },
+  {
+    pic: require('../assets/images/women9.jpg'),
+    title: 'Lisa, Défenseuse',
+    caption: 'Cherche une personne pour jouer les mecredi après midi',
+  },
+  {
+    pic: require('../assets/images/women10.jpg'),
+    title: 'Julia, Défenseuse',
+    caption: 'Hello',
+  },
+  {
+    pic: require('../assets/images/men1.jpg'),
+    title: 'Aaron, Attaquant',
+    caption: 'Je kiff attaquer',
+  },
+  {
+    pic: require('../assets/images/men2.jpg'),
+    title: 'Novak, Attaquant',
+    caption: 'Un ballon, un terrain, un match',
+  },
+  {
+    pic: require('../assets/images/men3.jpg'),
+    title: 'Justin, Attaquant',
+    caption: 'Attention je suis chaud',
+  },
+  {
+    pic: require('../assets/images/men4.jpg'),
+    title: 'Tony, Attaquant',
+    caption: 'Cherche partenaire',
+  },
+  {
+    pic: require('../assets/images/men5.jpg'),
+    title: 'Leo, Attaquant',
+    caption: 'Messi jr askip',
+  },
+  {
+    pic: require('../assets/images/men6.jpg'),
+    title: 'Ronald, Attaquant',
+    caption: 'Je suis les traces de Ronaldo',
+  },
+  {
+    pic: require('../assets/images/men7.jpg'),
+    title: 'Johnny, Défenseur',
+    caption: 'Foot is life',
+  },
+  {
+    pic: require('../assets/images/men8.jpg'),
+    title: 'Chandler, Défenseur',
+    caption: 'Défendre !',
+  },
+  {
+    pic: require('../assets/images/men9.jpg'),
+    title: 'Joey, Défenseur',
+    caption: 'Heeeey',
+  },
+  {
+    pic: require('../assets/images/men10.jpg'),
+    title: 'Alfie, Défenseur',
+    caption: 'Salut ! :)',
+  },
+])
--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Platform, SafeAreaView} from 'react-native';
+import { Tile } from 'react-native-elements';
+import Swiper from 'react-native-deck-swiper';
+import { Card } from '../components/Card'
+import { HomeScreenPics } from '../constants/Pics'
 
 export default class PostScreen extends React.Component {
     render () {
         return (
-            <View style={styles.container}>
-                <Text>Post Screen</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+        <Swiper
+          cards={HomeScreenPics}
+          renderCard={Card}
+          infinite // keep looping cards infinitely
+        //   backgroundColor="#3DD2B5"
+        backgroundColor="green"
+          cardHorizontalMargin={0}
+          stackSize={2} // number of cards shown in background
+          
+        />
+
+        
+      </SafeAreaView>
+      
         );
     }
 }
 
+
+
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
+        backgroundColor: 'transparent'
     }
 });
